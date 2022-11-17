e10be5a8c3c7c3ff6fc77f36ce29e06ce2beae32
momin <momin.thabet@gmail.com>
2022-11-17 11:23:47 +0200

    crud

--- routes/post.js
+++ routes/post.js
@@ -7,10 +7,14 @@ router.get('/',verify,async(req,res)=>{
     const posts = await Post.find({});
     res.status(200).send(posts);
 });
+
 router.get('/mine',verify,async(req,res)=>{
     const posts = await Post.find({user_id:req.user._id});
-    res.status(200).send(posts);
+    if(posts)
+        return res.status(200).send(posts);
+    res.status(400).send('user Dosen\'t exist');
 });
+
 router.post('/newpost',verify,async(req,res)=>{
     const {error} = postValidation({
         title:req.body.title,
@@ -30,7 +34,50 @@ router.post('/newpost',verify,async(req,res)=>{
     }catch(err){
         return res.status(400).send(err);
     }
-})
+});
+/// this need work 
+router.delete('/delete', verify , async(req,res)=>{
+    const post_id = req.body._id;
+    try{
+        const post = await Post.findById(post_id);
+        if(post){
+            if(post.user_id !== req.user._id) return res.status(400).send("unauthrized")
 
+            await post.delete();
+            res.status(200).send("deleted succefuly")
 
-module.exports =router;
+        }
+    }catch(error){
+        res.status(400).send(error);
+    }
+    // res.send({user:post.user_id===req.user._id})
+});
+
+router.post('/update',verify ,async(req,res)=>{
+    const {error} = postValidation({
+        title:req.body.title,
+        details:req.body.details
+    });
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const post_id = req.body._id;
+   
+    try{
+        const post = await Post.findById(post_id);
+        if(post){
+            if(post.user_id !== req.user._id) return res.status(400).send("unauthrized")
+        
+            await post.updateOne({
+                title:req.body.title,
+                details:req.body.details,
+                user_id :  req.user._id
+            });
+            res.status(200).send("updated succefuly")
+
+        }
+    }catch(error){
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router;
